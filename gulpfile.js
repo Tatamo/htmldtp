7f1f90f5eed7d631a703c22e7aa5d73aa2f87bf0
Tatamo <nsorder@gmail.com>
2017-11-09 17:32:02 +0900

    install browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 "use strict";
 const gulp = require("gulp");
-
 const nunjucks = require("nunjucks");
 const gulp_nunjucks = require('gulp-nunjucks');
 const nunjucks_markdown = require("nunjucks-markdown");
 const marked = require("marked");
 const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const cssnext = require("postcss-cssnext");
+const stylelint = require("stylelint");
+const browsersync = require("browser-sync").create();
 
 const nm_env = new nunjucks.Environment(new nunjucks.FileSystemLoader("src"));
 nunjucks_markdown.register(nm_env, marked);
@@ -14,16 +17,14 @@ gulp.task("nunjucks", () => {
 	return gulp.src(["src/**/*.@(njk|html)", "!src/**/_*.@(njk|html)"])
 		.pipe(gulp_nunjucks.compile("", {env: nm_env}))
 		.pipe(rename({extname: ".html"}))
-		.pipe(gulp.dest("dist/"));
+		.pipe(gulp.dest("dist/"))
+		.pipe(browsersync.stream());
 });
 
 gulp.task("watch-nunjucks", ["nunjucks"], () => {
 	gulp.watch(["src/**/*.@(njk|html)", "!src/**/_*.@(njk|html)"], ["nunjucks"]);
 });
 
-const postcss = require("gulp-postcss");
-const cssnext = require("postcss-cssnext");
-const stylelint = require("stylelint");
 
 gulp.task("postcss", () => {
 	const plugins = [
@@ -32,12 +33,19 @@ gulp.task("postcss", () => {
 	];
 	gulp.src("src/style/**/*.css")
 		.pipe(postcss(plugins))
-		.pipe(gulp.dest("dist/style"));
+		.pipe(gulp.dest("dist/style"))
+		.pipe(browsersync.stream());
 });
 
 gulp.task("watch-postcss", ["postcss"], () => {
 	gulp.watch("src/style/**/*.css", ["css"]);
 });
 
+gulp.task("browser-sync", () => {
+	browsersync.init({server: {baseDir: "./dist"}});
+});
+
 gulp.task("default", ["nunjucks", "postcss"]);
 gulp.task("watch", ["watch-nunjucks", "watch-postcss"]);
+gulp.task("serve", ["browser-sync", "watch"]);
+
